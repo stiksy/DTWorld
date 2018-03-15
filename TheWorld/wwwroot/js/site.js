2053ba6b43333a6dcf8cac0f7c407d158059666e
Fernando Levi Marquardt <live@stiksy.net>
2018-03-15 18:50:34 -0300

    .NET 2.0.6

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,18 +1,31 @@
 ﻿// site.js
 (function startup() {
-    var ele = $("#username")
-    ele.text("Fernando Levi Marquardt")
+    //var ele = $("#username")
+    //ele.text("Fernando Levi Marquardt")
 
-    var main = $("#main")
-    main.on("mouseenter", function () {
-        main.style.backgroundColor = "#888"
-    })
-    main.on("mouseleave", function () {
-        main.style.backgroundColor = ""
+    //var main = $("#main")
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888"
+    //})
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = ""
+    //})
+
+    //var menuitems = $("ul.menu li a")
+    //menuitems.on("click", function () {
+    //    var me = $(this)
+    //    alert(me.text())
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper")
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar")
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show menu")
+        } else {
+            $(this).text("Hide menu")
+        }
     })
 
-    var menuitems = $("ul.menu li a")
-    menuitems.on("click", function () {
-        alert("Hello")
-    })
 })()
